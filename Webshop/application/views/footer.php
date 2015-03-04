301522b0a11daf9db1b827ba92660df8606b2e60
KaiHerwijn <km.herwijn@student.avans.nl>
2015-03-04 13:47:24 +0100

    framework van anantgarg toegevoegd
    http://anantgarg.com/2009/03/30/write-your-own-php-mvc-framework-part-2/

--- Webshop/application/views/footer.php
+++ Webshop/application/views/footer.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html>
-<body>
-	<table>
-		<tr>
-			<td>/*privacy policy button*/</td>
-			<td>/* alg. voorwaarde button*/</td>
-			<td>/*veilig betalen*/</td>
-			<td>/*thuisbezorgt garantie*/</td>
-		</tr>
-	</table>
+
 </body>
 </html>
---
+++ Webshop/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/** Configuration Variables **/
+
+define ('DEVELOPMENT_ENVIRONMENT',true);
+
+
+define('DB_NAME', 'webshopclocks');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+
+define('BASE_PATH','http://localhost:8080/webshopClocks');
+
+
+define('PAGINATE_LIMIT', '5');
